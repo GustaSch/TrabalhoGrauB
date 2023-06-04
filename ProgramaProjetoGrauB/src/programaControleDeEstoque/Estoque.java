b239c9695fd4bc1eec2143729da99c806c5e8f23
Gustavo Ribeiro <gustavochiela@gmail.com>
2023-06-04 15:28:06 -0300

    Adicionado a classe Estoque, possuindo apenas um arraylist que recebe o objeto itens

---
+++ ProgramaProjetoGrauB/src/programaControleDeEstoque/Estoque.java
@@ -0,0 +1,19 @@
+package programaControleDeEstoque;
+
+import java.util.ArrayList;
+
+public class Estoque {
+    private ArrayList<Item> estoque = new ArrayList();
+
+    public Estoque(ArrayList<Item> estoque) {
+        this.estoque = estoque;
+    }
+
+    public ArrayList<Item> getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(ArrayList<Item> estoque) {
+        this.estoque = estoque;
+    }
+}
